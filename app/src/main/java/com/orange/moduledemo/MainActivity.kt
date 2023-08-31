231670db2adeb9cc29988e5c9b75a197cc9ae06e
UnnamedOrange <lycheng1215@sina.com>
2023-08-31 21:38:42 +0800

    Create empty project

---
+++ app/src/main/java/com/orange/moduledemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.orange.moduledemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.orange.moduledemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
