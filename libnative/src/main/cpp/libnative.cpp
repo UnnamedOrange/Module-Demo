e606119aa167bc2d509c832028ff2a6c185b2974
UnnamedOrange <lycheng1215@sina.com>
2023-09-01 09:40:07 +0800

    Leverage C++ module

--- libnative/src/main/cpp/libnative.cpp
+++ libnative/src/main/cpp/libnative.cpp
@@ -1,16 +1,18 @@
+module;
+
 #include <string>
-#include <concepts>
 
 #include <jni.h>
 
+export module libnative;
+
 using namespace std::literals;
 
-extern "C"
+export extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_orange_libnative_LibNativeKt_nativeAddHello(JNIEnv *env, jclass clazz, jstring original) {
     const auto str = env->GetStringUTFChars(original, nullptr);
     const auto ret = env->NewStringUTF(("Hello, "s + str).c_str());
-    static_assert(std::same_as<decltype(ret), const jstring>, "C++20 is not supported.");
     env->ReleaseStringUTFChars(original, str);
     return ret;
 }
